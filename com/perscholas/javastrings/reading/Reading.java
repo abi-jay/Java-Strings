8b180ac53638b6bcc1adf0658efc95642616b5de
Abhinaya Balaji <abijun4@gmail.com>
2022-11-28 00:25:56 -0800

    Adding hackerrank solutions

--- com/perscholas/javastrings/reading/Reading.java
+++ com/perscholas/javastrings/reading/Reading.java
@@ -13,7 +13,7 @@ public class Reading {
         System.out.println("s3 is " + s3);
         Scanner input1 = new Scanner(System.in);
         System.out.print("Enter a character: ");
-        String s = input.nextLine();
+        String s = input1.nextLine();
         char ch = s.charAt(0);
         System.out.println("The character entered is " + ch);
     }
